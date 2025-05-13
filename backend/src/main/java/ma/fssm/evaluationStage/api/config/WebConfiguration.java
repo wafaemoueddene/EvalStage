d157164a0fc62b9c8d482d1ebcb49002fb19dca8
amina-moueddene <a.moueddene0140@uca.ac.ma>
2025-05-13 20:01:51 +0100

    remove unused files from the project

--- backend/src/main/java/ma/fssm/evaluationStage/api/config/WebConfiguration.java
+++ backend/src/main/java/ma/fssm/evaluationStage/api/config/WebConfiguration.java
@@ -9,7 +9,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // URL of your React frontend
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
